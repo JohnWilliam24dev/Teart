2025d183f57fdc208c4ca40a8ef4e6fc17ff7b8c
JohnWilliam24dev <johnwilliam24.dev@gmail.com>
2025-03-23 13:37:41 -0300

    Produto & Servico implementation

---
+++ teart/src/main/java/com/duckers/teart/repositories/ServicoRepository.java
@@ -0,0 +1,40 @@
+package com.duckers.teart.repositories;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.duckers.teart.entities.Servico;
+import com.duckers.teart.repositories.Interfaces.ServicoJPA;
+@Repository
+public class ServicoRepository {
+    private ServicoJPA servicoRepository;
+    public ServicoRepository(ServicoJPA servicoRepository) {
+        this.servicoRepository = servicoRepository;
+    }
+
+    public void createPedido(Servico servico) {
+        servicoRepository.save(servico);
+    }
+
+    public Servico getServicoById(long id) {
+        return servicoRepository.findById(id).get();
+    }
+
+    public List<Servico> getAllServico() {
+        return servicoRepository.findAll();
+    }
+
+    public void updateServico(long id, Servico servicoAtualizado) {
+        Servico servicoPersistido = getServicoById(id);
+        servicoPersistido.setNome(servicoAtualizado.getNome());
+        servicoPersistido.setDescricao(servicoAtualizado.getDescricao());
+        servicoPersistido.setValoMinimo(servicoAtualizado.getValoMinimo());
+        servicoPersistido.setValorMaximo(servicoAtualizado.getValorMaximo());
+        servicoRepository.save(servicoPersistido);
+    }
+
+    public void deleteServico(long id) {
+        servicoRepository.deleteById(id);
+    }
+}
