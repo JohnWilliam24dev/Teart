037651ea31cda79554e08ec56086978688277848
JohnWilliam24dev <johnwilliam24.dev@gmail.com>
2025-04-21 19:29:02 -0300

    Utils

---
+++ teart/src/main/java/com/duckers/teart/entities/utils/Pessoa.java
@@ -0,0 +1,25 @@
+package com.duckers.teart.entities.utils;
+
+public class Pessoa {
+
+    // Pessoa.java
+
+
+    public void validaNome() {
+        // TODO
+    }
+
+    public void validaEmail() {
+        // TODO
+    }
+
+    public void validaTelefone() {
+        // TODO
+    }
+
+    public void validaCpf() {
+        // TODO
+    }
+
+
+}
